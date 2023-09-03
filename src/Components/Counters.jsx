5f166bed8f79c3211ae7b3298c73106041ce62d4
nereidarondon <nereida.rondon5@gmail.com>
2023-09-03 00:06:44 -0400

    centered counters in div

--- src/Components/Counters.jsx
+++ src/Components/Counters.jsx
@@ -1,23 +1,21 @@
+import { useState } from 'react';
+
 export default function Counters(props){
   
   const multiplier = props.multiplier;
   const multiplicand = props.multiplicand;
-
-  const line = () =>{
-    return(
-      <div>...</div>
-    );
-  }
+  
   const Counter = () =>{
-    console.log(multiplier)
-    
-    let counters='';
+    console.log(multiplier);
+    let counters = '';
     const designs = ['👽', '🚀', '⚪', '⭐', '🤖', '🪐', '🌎', '🛸' ];
     let index = Math.floor(Math.random()*8);
-    console.log(index);
-
     let design = designs[index];
-    for(let i=0; i < multiplicand; i++){
+    //const [design, setDesign] = useState('');
+    console.log(index);
+    
+
+    for(let j=0; j < multiplicand; j++){
       
       counters += ` ${design} `;
     }
@@ -25,19 +23,18 @@ export default function Counters(props){
     const allRows=[];
     for(let i=1; i <= multiplier; i++){
       allRows.push([i, counters]) ;
-      console.log(allRows);
     }
   
-    const num2x = allRows.map((x) => {
+    const groups = allRows.map((x) => {
       return(
-        <div className='flex content-center justify-center items-center' key={x[0]}>
+        <div className='flex flex-wrap content-center justify-center items-center' key={x[0]}>
           {/* <div className='my-1 mx-2 pt-3 w-16 h-16 bg-black rounded-full text-center'>{x[0]}</div> */}
-          <div className='md:p-6 m-4 w-32 h-32 md:w-48 md:h-48 border-2 border-teal-500 rounded-full text-center' >{x[1]}</div> 
+          <div className='flex flex-wrap p-3 sm:p-6 m-2 sm:m-4 w-32 h-32 sm:w-48 sm:h-48 border-2 border-teal-500 rounded-full text-center content-center justify-center' ><p className='text-center m-auto '>{x[1]}</p></div> 
         </div>
       );
     });
 
-    return (num2x);
+    return (groups);
   }
 
   return(
