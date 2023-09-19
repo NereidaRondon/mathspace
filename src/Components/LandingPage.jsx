8429760cf2e2306b0279f657c9f1e4178607da6c
nereidarondon <nereida.rondon5@gmail.com>
2023-09-18 22:43:09 -0400

    Change the landing page button layout and add icons to action buttons

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import astronaut from '../assets/astronaut.svg';
 
 export default function MathSpace(){
-  const buttonClass = 'rounded-xl md:rounded-2xl text-2xl md:text-4xl mx-4 lg:mx-10 my-16 py-4 md:py-6 w-36 md:w-48 font-bold text-white btn-animation glow-on-hover';
+  const buttonClass = 'rounded-xl md:rounded-2xl text-2xl md:text-4xl mx-5 lg:mx-10 my-5 py-4 md:py-6 w-36 md:w-48 font-bold text-white btn-animation glow-on-hover';
 
   return(
     <>
@@ -10,9 +10,10 @@ export default function MathSpace(){
       
       <h1 className="font-gugi w-11/12 mt-20 mb-60 md:mb-80 text-5xl md:text-7xl lg:text-9xl drop-shadow-5xl text-white title">MathSpace</h1>
       
-      <img id='astronaut' src={astronaut} className='absolute top-36 md:top-44 lg:top-40 w-5/12 md:w-2/5 lg:w-1/3 xl:w-1/4' alt='An astronaut floating in space among yellow stars with a yellow star in their palm'/>
+      <img id='astronaut' src={astronaut} className='absolute top-36 md:top-44 lg:top-40 w-56 sm:w-2/5 lg:w-1/3 xl:w-1/4' alt='An astronaut floating in space among yellow stars with a yellow star in their palm'/>
 
-      <section className="lg:mt-10">
+      <section className="sm:w-96 md:w-3/5 xl:w-screen -mt-6 sm:mt-10 md:mt-15 xl:mt-24">
+        
         <Link to='/numbersmenu'><button className={buttonClass}>Numbers</button></Link>
 
         <Link to='/additionmenu'><button className={buttonClass}>Add</button></Link>
@@ -23,7 +24,6 @@ export default function MathSpace(){
 
       </section>
       
-
     </main>
     </>
   );
--- src/Components/multiply/ActionButtons.jsx
+++ src/Components/multiply/ActionButtons.jsx
@@ -1,4 +1,6 @@
 //import { Link , useNavigate} from "react-router-dom";
+import check from '../../assets/check-solid.svg';
+import arrow from '../../assets/arrow-right-solid.svg';
 
 export default function ActionButtons(props){
   let showCCBtn = props.showCheckClearBtn;
@@ -9,15 +11,17 @@ export default function ActionButtons(props){
     <div className='flex flex-wrap justify-around w-48 md:w-72 py-1'>
       
       <div className='w-24 h-24'>
-        {showCCBtn && <button onClick={props.clear} className='border border-teal-100 text-2xl text-black w-20 h-16 md:h-20 px-2 m-2 bg-sky-500 rounded-full'>clear</button>}
+        {showCCBtn && <button onClick={props.clear} className='border border-teal-100 text-xl md:text-3xl text-black w-16 h-16 md:w-20 md:h-20 px-1 py-1 m-2 bg-sky-500 rounded-full font-bold'>clear</button>}
       </div>
 
       <div className='flex flex-wrap w-25'>
-        {showCCBtn && <button onClick={props.check} className='border border-green-300 text-2xl w-16 h-16 md:w-20 md:h-20 px-1 py-1 m-2 bg-green-500 rounded-full text-center'>✔️</button>}
+        {showCCBtn && <button onClick={props.check} className='border border-green-300 text-2xl w-16 h-16 md:w-20 md:h-20 px-1 py-1 m-2 bg-green-500 rounded-full text-center'>
+          <img src={check} className='w-12 h-12 text-center m-auto'/></button>}
 
-        {showNext && <button onClick={props.next} className='border border-blue-300 text-3xl w-16 h-16 md:w-20 md:h-20 px-1 py-1 m-2 bg-sky-500 rounded-full text-center'>➡️</button>}
+        {showNext && <button onClick={props.next} className='border border-blue-300 text-3xl w-16 h-16 md:w-20 md:h-20 px-1 py-1 m-2 bg-sky-500 rounded-full text-center'>
+          <img src={arrow} className='w-12 h-12 text-center m-auto'/></button>}
 
-        {exit && <button onClick={props.exit} className='border border-teal-900 text-3xl w-36 px-2 py-5 m-4 bg-teal-300 text-black rounded-xl text-center'>Exit</button>}
+        {exit && <button onClick={props.exit} className='border border-teal-900 text-2xl md:text-3xl w-16 h-16 md:w-20 md:h-20 px-2 py-1 m-2 bg-teal-300 text-black rounded-full text-center font-bold'>Exit</button>}
       </div>
 
         
