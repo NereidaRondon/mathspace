cdca24dc59cd69ccf5a9eff12440efc32126aa22
nereidarondon <nereida.rondon5@gmail.com>
2023-10-15 22:16:13 -0400

    change size of multiply menu on small screen

--- src/Components/HomeButton.jsx
+++ src/Components/HomeButton.jsx
@@ -4,7 +4,7 @@ import rocket from '../assets/mathspace-logo.webp';
 export default function HomeButton(){
   return(      
        <Link to='/'> 
-         <img src={rocket} className='rocket absolute top-3 md:top-5 left-3 md:left-5  md:w-15 h-15 md:h-50 transition-all' alt='Back to Home Page'/>
+         <img src={rocket} className='rocket absolute top-3 md:top-5 left-3 md:left-5  md:w-15 h-20 md:h-24 transition-all' alt='Back to Home Page'/>
        </Link>
   );
 }  
--- src/Components/multiply/MultiplyMenu.jsx
+++ src/Components/multiply/MultiplyMenu.jsx
@@ -2,7 +2,7 @@ function MultiplyMenu(props){
 
   return(
 
-    <section className='z-10 w-10/12 lg:w-3/4 h-4/5 lg:h-3/4 absolute top-24 md:top-20 left-0 right-0 m-auto p-1 text-xl md:text-3xl text-center start-modal rounded-2xl border border-teal-800 bg-mygradient'>
+    <section className='z-10 w-3/4 h-4/5 lg:h-3/4 absolute top-24 md:top-20 left-0 right-0 m-auto p-1 text-xl md:text-3xl text-center start-modal rounded-2xl border border-teal-800 bg-mygradient'>
       <h2 className='my-8'>
         What multiplier do you want to practice?
       </h2>
